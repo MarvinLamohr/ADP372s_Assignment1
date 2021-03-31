bac925ea6d34099624dcb1b584b6ebca3f92dfa0
MARVIN <marvinlamohr@outlook.com>
2021-03-31 13:01:07 +0200

    Working Solution

--- src/main/java/za/ac/cput/ADP372s_Assignment1/TestDrivenDevelopment.java
+++ src/main/java/za/ac/cput/ADP372s_Assignment1/TestDrivenDevelopment.java
@@ -1,4 +1,41 @@
 package za.ac.cput.ADP372s_Assignment1;
 
 public class TestDrivenDevelopment {
+
+    private String carBrand,carType,carColour;
+
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public String getCarColour() {
+        return carColour;
+    }
+
+    public void setCarColour(String carColour) {
+        this.carColour = carColour;
+    }
+
+
+    @Override
+    public String toString() {
+        return "TestDrivenDevelopment{" +
+                "carBrand='" + carBrand + '\'' +
+                ", carType='" + carType + '\'' +
+                ", carColour='" + carColour + '\'' +
+                '}';
+    }
 }
--- src/test/java/za/ac/cput/ADP372s_Assignment1/TestDrivenDevelopmentTest.java
+++ src/test/java/za/ac/cput/ADP372s_Assignment1/TestDrivenDevelopmentTest.java
@@ -1,6 +1,10 @@
 package za.ac.cput.ADP372s_Assignment1;
 
-/*
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
 
  * Student Name: MARVIN LAMOHR
  * Student Number: 214273830
@@ -9,6 +13,81 @@ package za.ac.cput.ADP372s_Assignment1;
 
 class TestDrivenDevelopmentTest {
 
+    private TestDrivenDevelopmentTest car1;
+    private TestDrivenDevelopmentTest car2;
+    private TestDrivenDevelopmentTest car3;
+    private TestDrivenDevelopmentTest car4;
+
+    private TestDrivenDevelopmentTest shop1;
+    private TestDrivenDevelopmentTest shop2;
+    private TestDrivenDevelopmentTest shop3;
+
+
+    @BeforeEach
+    void setUp(){
+
+        car1 = new TestDrivenDevelopmentTest();
+        car2 = new TestDrivenDevelopmentTest();
+        car3 = new TestDrivenDevelopmentTest();
+        car4 = car2;
+
+
+        shop1 = new TestDrivenDevelopmentTest();
+        shop2 = shop3;
+
+    }
+
+
+    /* Testing For Object Equality */
+
+    @Test
+    void objectEquality(){
+
+        assertEquals(car1,car1);
+    }
+
+
+    /* Testing For Object Identity */
+
+    @Test
+    void objectIdentity(){
+
+        assertSame(car2,car4);
+
+    }
+
+
+    /* Making Sure The Test Fails */
+
+    @Test
+    void failingTest(){
+
+        fail("Loadshedding...Shutting Down...");
+        assertNotSame(shop1,car1);
+
+    }
+
+
+    /* Running Timeouts */
+
+    @Test
+    @Timeout(3)
+    void timeOuts() throws InterruptedException {
+
+    Thread.sleep(300);
+    System.out.println("Test will pass soon... Please be patient...");
+    }
+
+
+    /* Initiating A Disabling Test */
+
+    @Test
+    @Disabled
+    void disablingTest() throws InterruptedException{
+
+        assertEquals(shop2,shop3);
+        System.out.println("Exit");
+    }
 
 
 
